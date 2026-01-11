b7b0331dfea145df62e705c605c8aed59fbb475a
Christian Andrei Reyes <rchristianandrei@gmail.com>
2026-01-11 15:45:59 +0800

    create simple header

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,40 +1,20 @@
-import { useState } from "react";
+import { useAuth } from "../context/AuthProvider";
 import Tasks from "../Tasks";
-import Users from "../Users";
 
 export function Home() {
-  const [currentTab, setCurrentTab] = useState("tasks");
-
-  function changeTab(tab) {
-    if (currentTab === tab) return;
-    setCurrentTab(tab);
-  }
+  const { user } = useAuth();
 
   return (
     <div className="app h-screen flex flex-col gap-3">
-      <h1>📝 React Task Evaluator</h1>
-      <nav>
-        <ul className="flex items-center justify-center gap-10 border-b max-w-300 mx-auto *:text-lg *:cursor-pointer">
-          <li
-            className={
-              currentTab === "tasks" ? "border-b-5 border-blue-500" : ""
-            }
-            onClick={() => changeTab("tasks")}
-          >
-            Tasks
-          </li>
-          <li
-            className={
-              currentTab === "users" ? "border-b-5 border-blue-500" : ""
-            }
-            onClick={() => changeTab("users")}
-          >
-            Users
-          </li>
-        </ul>
-      </nav>
-      {currentTab === "tasks" && <Tasks className="flex-1" />}
-      {currentTab === "users" && <Users className="flex-1" />}
+      <header className="flex items-center justify-between p-5 border-b relative lg:static">
+        <h1 className="text-lg md:text-2xl cursor-pointer">
+          📝 React Task Evaluator
+        </h1>
+        <div className="group lg:relative cursor-pointer">
+          <span>{user.email}</span>
+        </div>
+      </header>
+      <Tasks className="flex-1" />
     </div>
   );
 }
